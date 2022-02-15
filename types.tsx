79d6f7282fe4920db7d08677926b7765b7b29cb3
Yarecky <arek.erduk@gmail.com>
2022-02-15 18:55:56 +0100

    Initial commit

--- types.tsx
+++ types.tsx
@@ -25,8 +25,11 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 >;
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  MyList: undefined;
+  Top: undefined;
+  Search: undefined;
+  Profile: undefined;
+  Settings: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<

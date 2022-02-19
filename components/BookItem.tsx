0676d6605b10351c35ebee9201aa75f511850a44
Yarecky <arek.erduk@gmail.com>
2022-02-19 10:41:03 +0100

    krzys

---
+++ components/BookItem.tsx
@@ -0,0 +1,14 @@
+import * as WebBrowser from 'expo-web-browser';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+import Colors from '../constants/Colors';
+import { MonoText } from './StyledText';
+import { Text, View } from './Themed';
+
+export default function BookItem() {
+    return (
+        <view>
+            <Text>BOOK ITEM</Text>
+        </view>
+    )
+}
